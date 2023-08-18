975d3dcffcae6bc147cd4e9ad04dc7634521829b
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2023-08-18 23:37:11 +0900

    Add: Multi-Processing use python

---
+++ Python_Study/PythonTutorial/multiprocessing/multiprocessing_tutorial.py
@@ -0,0 +1,31 @@
+from multiprocessing import Process
+import time
+import logging
+
+
+FORMAT = "%(asctime)s: %(message)s"
+logging.basicConfig(format=FORMAT, level=logging.INFO, datefmt="%H:%M:%S")
+
+
+def proc(name):
+    print(f"Sub Process start >> {name}")
+    time.sleep(3)
+    print(f"Sub Process finish >> {name}")
+
+
+def main():
+    p = Process(target=proc, args=("First", ))
+    logging.info("Main Process: before create process")
+
+    p.start()
+
+    logging.info("Main Process running")
+    logging.info("Main Process terminated")
+    p.terminate()
+    logging.info("Main Process wait for finish sub-process")
+    p.join()
+    print(f"Process p is alive: {p.is_alive()}")
+
+
+if __name__ == "__main__":
+    main()
